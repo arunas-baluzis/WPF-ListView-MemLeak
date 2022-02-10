abd504cf5ffc22c0cc7bec06f4a4014cd6087bb1
Arūnas Balužis <arunas.baluzis@proton.ch>
2022-02-10 16:14:01 +0200

    Add sample app

---
+++ ItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MemLeak
+{
+    public class ItemViewModel : INotifyPropertyChanged
+    {
+        public ItemViewModel(int value)
+        {
+            Value = value;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int Value { get; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MemLeak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Dispatcher _dispatcher = Dispatcher.CurrentDispatcher;
+        private readonly Lazy<Task> _work;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _work = new Lazy<Task>(DoWork);
+            DataContext = this;
+        }
+
+        public ObservableCollection<ItemViewModel> Items { get; } = new();
+
+        protected override void OnActivated(EventArgs e)
+        {
+            _ = _work.Value;
+
+            base.OnActivated(e);
+        }
+
+        private async Task DoWork()
+        {
+            AddItems(200);
+
+            for (var i = 0; i < 2000; i++)
+            {
+                await Task.Delay(15);
+
+                _dispatcher.Invoke(() => MoveItems(57));
+            }
+        }
+
+        private void AddItems(int numberOfItems)
+        {
+            // The list is filled with items
+            for (var i = 0; i < numberOfItems; i++)
+            {
+                Items.Add(new ItemViewModel(i));
+            }
+        }
+
+        private void MoveItems(int numberOfItems)
+        {
+            for (var i = 0; i < numberOfItems; i++)
+            {
+                // Last item is moved to the top of the list
+                Items.Move(Items.Count - 1, 0);
+            }
+        }
+    }
+}
